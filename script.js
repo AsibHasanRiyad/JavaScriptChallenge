32c646dc1be2f7eb4bbc5f5159a53ff88d65dc0c
Sayad Riyad <sayadriyad@Sayads-MacBook-Air.local>
2024-01-03 13:11:51 +0600

    challenge 2 is completed

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ Test data:
 Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
 Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
 
-GOOD LUCK */
+GOOD LUCK 
 
 const marksWeights = 78;
 const marksHeight = 1.69;
@@ -42,3 +42,40 @@ const johnsBMI = johnsWeights / johnsHeight ** 2;
 console.log(marksBMI, johnsBMI);
 const markHigherBMI = marksBMI > johnsBMI;
 console.log(markHigherBMI);
+
+
+const age = 17;
+// const isOldEnough = age >= 18;
+if (age >= 18) {
+  console.log("I am enable for Driving License 🚗");
+} else {
+  console.log("Not enable");
+}
+
+
+const birthYear = 1998;
+let century;
+if (birthYear >= 2000) {
+  century = 21;
+} else {
+  century = 20;
+}
+console.log(century);
+*/
+
+/*
+1. Print a nice output to the console, saying who has the higher BMI. The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+*/
+const marksWeights = 78;
+const marksHeight = 1.69;
+const johnsWeights = 92;
+const johnsHeight = 1.95;
+const marksBMI = marksWeights / marksHeight ** 2;
+const johnsBMI = johnsHeight / johnsHeight ** 2;
+if (marksBMI > johnsBMI) {
+  console.log(`Mark's BMI ${marksBMI} is higher than John's BMI ${johnsBMI}`);
+} else {
+  console.log(`John's BMI ${johnsBMI} is higher than Mark's BMI ${marksBMI}`);
+}
